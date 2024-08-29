5430075e99218c72f7db06f9ed1bfb707e8bfac8
Katrina Gorska <115571613+JekaterinaGorska@users.noreply.github.com>
2024-08-30 00:04:09 +0100

    player animation

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -13,15 +13,38 @@
             this.y = y;
             this.spriteSheet = spriteSheet;
             this.speed = 5;
+            this.scaleFactor = 2.0;
+
+        this.frameIndex = 0;
+        this.frameWidth = 64;
+        this.frameHeight = 64;
+        this.totalFrames = 4;
+        this.animationSpeed = 5;
+        this.frameCounter = 0;
+        this.markForDeletion = false;
     }
     update(){
         this.x += this.speed;
         if(this.x > canvas.width){
-            delete this;
+            this.markForDeletion = true;
+        }
+
+        this.frameCounter++;
+        if(this.frameCounter >= this.animationSpeed)
+        {
+            this.frameCounter = 0;
+            this.frameCounter = (this.frameIndex + 1) % this.totalFrames;
         }
     }
     draw(ctx){
-        ctx.drawImage(this.spriteSheet, this.x, this.y);
+        const sourceX = this.frameIndex * this.frameWidth;
+        ctx.drawImage(
+            this.spriteSheet,
+            sourceX, 0,
+            this.frameWidth, this.frameHeight,
+            this.x, this.y,
+            this.frameWidth * this.scaleFactor, this.frameHeight * this.scaleFactor
+        );
     }
 }
 
@@ -37,6 +60,16 @@
 
             this.projectiles = [];
 
+            this.frameIndex = 0; 
+            this.totalFrames = 4; 
+            this.frameWidth = 600 / this.totalFrames; 
+            this.frameHeight = 100; 
+            this.animationSpeed = 10; 
+            this.frameCounter = 0; 
+            this.animationDirection = 1;
+            this.facingDirection = 'right';
+
+
             this.handleKeyDown = this.handleKeyDown.bind(this);
             this.handleKeyUp = this.handleKeyUp.bind(this);
             this.shoot = this.shoot.bind(this);
@@ -44,12 +77,24 @@
     
             window.addEventListener('keydown', this.handleKeyDown);
             window.addEventListener('keyup', this.handleKeyUp);
-            window.addEventListener('click', this.shoot);
+            //window.addEventListener('click', this.shoot);
         }
-        shoot(event){
-            console.log(event)
-            this.projectiles.push(new Projectile(this.x, this.y, this.spriteSheet));
-        }
+        shoot(){
+            const projectileX = this.x + this.frameWidth / 2; 
+            const projectileY = this.y; 
+            const projectileOffset = this.frameWidth / 2; 
+
+
+            if (this.facingDirection === 'right') {
+                this.projectiles.push(new Projectile(projectileX + projectileOffset, projectileY, projectileSpritesheet));
+            } else if (this.facingDirection === 'left') {
+                this.projectiles.push(new Projectile(projectileX - projectileOffset, projectileY, projectileSpritesheet));
+            } else if (this.facingDirection === 'up') {
+                this.projectiles.push(new Projectile(projectileX, projectileY - projectileOffset, projectileSpritesheet));
+            } else if (this.facingDirection === 'down') {
+                this.projectiles.push(new Projectile(projectileX, projectileY + projectileOffset, projectileSpritesheet));
+            }
+            }
       
         handleKeyDown(event){
             switch(event.key){
@@ -61,9 +106,14 @@
                     break;
                 case 'a':
                     this.vx = -this.speed;
+                    //this.animationDirection = -1;
                     break;
                 case 'd':
                     this.vx = this.speed;
+                    //this.animationDirection = 1;
+                    break;
+                case ' ':
+                    this.shoot();
                     break;
                 default:
                     break;
@@ -91,22 +141,53 @@
         update(){
             this.x += this.vx;
             this.y += this.vy;
-            this.pushBack();
+            //this.pushBack();
 
+            if (this.vx !== 0 || this.vy !== 0) { 
+                this.frameCounter++;
+                if (this.frameCounter >= this.animationSpeed) {
+                    this.frameCounter = 0;
+                    this.frameIndex = (this.frameIndex + 1) % this.totalFrames;
+                }
+            } else {
+                this.frameIndex = 0; 
+            }
+    
+
+
+            if(this.x < 0){
+                this.x = 0;
+            }else if (this.x + this.spriteSheet.width > canvas.width){
+                this.x = canvas.width - this.spriteSheet.width;
+            }
+            if(this.y < 0){
+                this.y = 0;
+            }else if (this.y + this.spriteSheet.height > canvas.height){
+                this.y = canvas.height - this.spriteSheet.height;
+            }
+            
             this.projectiles.forEach(projectile => {
                 projectile.update();
             });
+            this.projectiles = this.projectiles.filter(projectile => !projectile.markForDeletion);
         }
 
-        pushBack(){
-            this.x -= 0.5;
-        }
+        // pushBack(){
+        //     this.x -= 0.5;
+        // }
     
         draw(ctx){
-            ctx.drawImage(this.spriteSheet, this.x, this.y);
-            this.projectiles.forEach(projectile => {
-                projectile.draw(ctx);
-            });
+            const sourceX = this.frameIndex * this.frameWidth;
+            const scaleFactor = 2.0;
+
+            ctx.drawImage(
+                this.spriteSheet, 
+                sourceX, 0, 
+                this.frameWidth, this.frameHeight, 
+                this.x, this.y, 
+                this.frameWidth * scaleFactor, this.frameHeight * scaleFactor
+            );
+            this.projectiles.forEach(projectile => projectile.draw(ctx));
         }
     }
     
@@ -115,7 +196,10 @@
     backgroundImage.src = 'ASSETS/IMG/background.jpeg';
     
     const playerImage = new Image();
-    playerImage.src = 'ASSETS/IMG/griffon1.png';
+    playerImage.src = 'ASSETS/IMG/griffonSpriteSheet.png';
+
+    const projectileSpritesheet = new Image();
+    projectileSpritesheet.src = 'ASSETS/IMG/projectile.png';
 
 
 
